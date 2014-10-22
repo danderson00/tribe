422afe9a4d07ef43d85c670ae9106981965cb523
danderson00 <danderson00@gmail.com>
2014-10-22 16:16:28 +1100

    Added sqlite3 implementation and tests

---
+++ node_modules/tribe.storage/sqlite3/entityContainer.js
@@ -0,0 +1,24 @@
+﻿var queries = require('./queries'),
+    _ = require('underscore'),
+    Q = require('q');
+
+module.exports = function (name, indexes, database) {
+    return {
+        store: function (entities) {
+            if (entities.constructor === Array)
+                return Q.all(_.map(entities, storeEntity));
+            return storeEntity(entities);
+
+            function storeEntity(entity) {
+                return database.run(queries.store(name, indexes, entity));
+            }
+        },
+        retrieve: function (predicates) {
+            return database.all(queries.retrieve(name, predicates)).then(function (rows) {
+                return _.map(rows, function (row) {
+                    return JSON.parse(row.__content);
+                });
+            });
+        }
+    };
+}
---
+++ node_modules/tribe.storage/tests/sqlite3/queries.tests.js
@@ -0,0 +1,35 @@
+﻿suite('tribe.storage.sqlite3.queries', function () {
+    var queries = require('tribe.storage/sqlite3/queries');
+
+    test("store simple envelope with no indexes", function () {
+        var envelope = { id: 1, topic: 'test' },
+            query = queries.store('messages', [], envelope);
+        expect(query.sql).to.equal("insert into messages (__content) values (?)");
+        expect(query.parameters).to.deep.equal([JSON.stringify(envelope)]);
+    });
+
+    test("store envelope with indexes", function () {
+        var envelope = { id: 1, topic: 'test', value1: 1, value2: 2 },
+            query = queries.store('messages', ['value1', 'value2'], envelope);
+        expect(query.sql).to.equal("insert into messages (__content,value1,value2) values (?,?,?)");
+        expect(query.parameters).to.deep.equal([JSON.stringify(envelope), 1, 2]);
+    });
+
+    test("retrieve with index", function () {
+        var query = queries.retrieve('messages', { p: 'testId', v: 1 });
+        expect(query.sql).to.equal('SELECT * FROM messages WHERE testId = ?');
+        expect(query.parameters).to.deep.equal([1]);
+    });
+
+    test("retrieve with array indexValue", function () {
+        var query = queries.retrieve('messages', { p: 'testId', v: [1, 2] });
+        expect(query.sql).to.equal('SELECT * FROM messages WHERE testId IN (?,?)');
+        expect(query.parameters).to.deep.equal([1, 2]);
+    });
+
+    test("retrieve with multiple indexes", function () {
+        var query = queries.retrieve('messages', [{ p: 'testId', v: 1 }, { p: 'id', o: '>=', v: 10 }]);
+        expect(query.sql).to.equal('SELECT * FROM messages WHERE testId = ? AND id >= ?');
+        expect(query.parameters).to.deep.equal([1, 10]);
+    });
+});
