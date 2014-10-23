4d730af0c04b62907b3884b1720a910cd4f9a963
danderson00 <danderson00@gmail.com>
2014-10-23 22:28:27 +1100

    Updated tribe.pubsub to use expressions

---
+++ node_modules/tribe.expressions/evaluate.js
@@ -0,0 +1,19 @@
+﻿// Array.indexOf polyfill
+require('./indexOf');
+
+var keyPath = require('./keyPath');
+
+module.exports = function (expression, target) {
+    var value = keyPath(expression.p, target);
+    switch (expression.o) {
+        case undefined:
+        case '=': return value === expression.v;
+        case '!=': return value !== expression.v;
+        case '<': return value < expression.v;
+        case '<=': return value <= expression.v;
+        case '>': return value > expression.v;
+        case '>=': return value >= expression.v;
+        case 'in': return expression.v.indexOf(value) > -1;
+        case 'contains': return value.indexOf(expression.v) > -1;
+    }
+}
