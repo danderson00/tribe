6c9b5d5ab6a7058f19bb9d0db77f4f875b08a96b
danderson00 <danderson00@gmail.com>
2014-12-02 00:06:33 +1100

    Added indexeddb raw operations and metadata

---
+++ node_modules/tribe.storage/indexeddb/upgrade.js
@@ -0,0 +1,32 @@
+﻿module.exports = function (entities, previousEntities) {
+    return function (database, transaction) {
+        for(var i = 0, l = entities.length; i < l; i++) {
+            var entity = entities[i],
+                store = retriveStore(entity.name);
+
+            if(entity.indexes)
+                for (var j = 0, l2 = entity.indexes.length; j < l2; j++)
+                    createIndex(store, entity.indexes[j]);
+        }
+
+        function retriveStore(name) {
+            if (!database.objectStoreNames.contains(name))
+                return database.createObjectStore(name, { autoIncrement: true });
+
+            return transaction.objectStore(name);
+        }
+
+        function createIndex(store, index) {
+            var name = indexName(index);
+
+            if (!store.indexNames.contains(name))
+                store.createIndex(name, index, { unique: false });
+        }
+    };
+}
+
+function indexName(index) {
+    if (index.constructor === Array)
+        return index.join('_');
+    return index;
+}
