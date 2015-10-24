b49c2dbcd8a94ab662b0842bdc35eca67c29e76f
Dale Anderson <danderson00@gmail.com>
2015-10-24 16:13:59 -0700

    Added query function to rx model

--- samples/score/flows/game.js
+++ samples/score/flows/game.js
@@ -1,11 +1,11 @@
 require('tribe').register.flow(function (flow) {
     flow.isDistributed()
 
-    flow.startsAt('selectPlayer')
+    flow.startsAt({ path: 'selectPlayer', scope: 'reset' })
 
     flow.topic('player.selected')
         .count()
-        .when(1).then(flow.to('selectPlayer'))
+        .when(1).then(flow.to({ path: 'selectPlayer', scope: 'reset' }))
         .when(2).then(flow.to('score'))
 
     flow.on('game.complete').to('complete')
