0295b372285192e21e68fb918ea21eacb03b9201
Dale Anderson <danderson00@gmail.com>
2015-10-24 17:50:25 -0700

    Add ability to query by topic and scope

--- samples/score/flows/game.js
+++ samples/score/flows/game.js
@@ -1,11 +1,11 @@
 require('tribe').register.flow(function (flow) {
     flow.isDistributed()
 
-    flow.startsAt({ path: 'selectPlayer', scope: 'reset' })
+    flow.startsAt({ path: 'selectPlayer' })
 
     flow.topic('player.selected')
         .count()
-        .when(1).then(flow.to({ path: 'selectPlayer', scope: 'reset' }))
+        .when(1).then(flow.to({ path: 'selectPlayer' }))
         .when(2).then(flow.to('score'))
 
     flow.on('game.complete').to('complete')
