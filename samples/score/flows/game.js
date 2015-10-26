cf81197ffe799d0b5bf1df505fd98752a81bc80c
Dale Anderson <danderson00@gmail.com>
2015-10-25 17:42:05 -0700

    Refined styling

--- samples/score/flows/game.js
+++ samples/score/flows/game.js
@@ -1,11 +1,11 @@
 require('tribe').register.flow(function (flow) {
     flow.isDistributed()
 
-    flow.startsAt({ path: 'selectPlayer' })
+    flow.startsAt({ path: 'selectPlayer', data: { heading: 'Select Player 1' } })
 
     flow.topic('player.selected')
         .count()
-        .when(1).then(flow.to({ path: 'selectPlayer' }))
+        .when(1).then(flow.to({ path: 'selectPlayer', data: { heading: 'Select Player 2' } }))
         .when(2).then(flow.to('score'))
 
     flow.on('game.won').to('complete')
