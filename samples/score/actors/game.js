fbe91d7ee1cb4e35e6958e90bd5f40d9411772b5
Dale Anderson <danderson00@gmail.com>
2015-10-25 18:19:14 -0700

    rx foreach override is broken - requires debug ko build. Added home buttons.

--- samples/score/actors/game.js
+++ samples/score/actors/game.js
@@ -1,5 +1,6 @@
 require('tribe').register.actor(function (actor) {
     actor.envelopes.players()
-        .forEach(x => x.points().when(3)
+        .forEach(x => x.points()
+            .when(x => x >= 3)
             .then(() => actor.publish('game.won', { playerId: x.key })))
 })
