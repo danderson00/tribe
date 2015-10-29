b618bdaca9ad2dbbf3c8fc98aa59f4c206af5d60
Dale Anderson <danderson00@gmail.com>
2015-10-28 23:39:12 -0700

    Added readme

--- samples/score/actors/game.js
+++ samples/score/actors/game.js
@@ -1,6 +1,12 @@
 require('tribe').register.actor(function (actor) {
+    actor.isDistributed()
+
+    this.points = actor.envelopes.players().select(x => x.points().asScalar()).asArray()
+
     actor.envelopes.players()
         .forEach(x => x.points()
-            .when(x => x >= 3)
+            //.when(3)
+            // this is rather nasty. a much, much neater expression coming soon...
+            .when(x => x >= 3 && this.points().every(y => y() === x || y() < x - 1) && (this.points().length === 1 || !this.points().every(y => y() === x)))
             .then(() => actor.publish('game.won', { playerId: x.key })))
 })
