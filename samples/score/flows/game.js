825557fc52b39b12b160c4eaeb645ba52d711022
Dale Anderson <danderson00@gmail.com>
2015-11-22 19:39:07 -0800

    Added data parameter to flow.to

--- samples/score/flows/game.js
+++ samples/score/flows/game.js
@@ -1,11 +1,11 @@
 require('tribe').register.flow(function (flow) {
     flow.isDistributed()
 
-    flow.startsAt({ path: 'selectPlayer', data: { heading: 'Select Player 1' } })
+    flow.startsAt('selectPlayer', { heading: 'Select Player 1' })
 
     flow.topic('player.selected')
         .count()
-        .when(1).then(flow.to({ path: 'selectPlayer', data: { heading: 'Select Player 2' } }))
+        .when(1).then(flow.to('selectPlayer', { heading: 'Select Player 2' }))
         .when(2).then(flow.to('score'))
 
     flow.on('game.won').to('complete')
